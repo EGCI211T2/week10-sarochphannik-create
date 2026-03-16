8befd2b5e73133e447117ce408103f810e0026ca
Sin(guy)/Cos(guy) <sarochphan.nik@student.mahidol.edu>
2026-03-16 08:37:45 +0000

    codes

--- Time.h
+++ Time.h
@@ -2,9 +2,33 @@ struct Time{
 	int m,h,s;
 };
 
+// 1) Read a time in format: h m s
+void getTime(Time &t) {
+	cin >> t.h >> t.m >> t.s;
+}
 
-//create 3 functions
+// 2) Subtract two times: result = t1 - t2, wrapped in 24 hours
+Time subtract(Time t1, Time t2) {
+	const int daySeconds = 24 * 60 * 60;
+	int sec1 = t1.h * 3600 + t1.m * 60 + t1.s;
+	int sec2 = t2.h * 3600 + t2.m * 60 + t2.s;
 
-//1.  getTime function
-//2. subtract fuctions
-//3. display functions
+	int diff = sec1 - sec2;
+	if (diff < 0) {
+		diff += daySeconds;
+	}
+
+	Time t3;
+	t3.h = diff / 3600;
+	diff %= 3600;
+	t3.m = diff / 60;
+	t3.s = diff % 60;
+	return t3;
+}
+
+// 3) Display in format 00:00:00
+void display(Time t) {
+	cout << setfill('0') << setw(2) << t.h << ":"
+		 << setw(2) << t.m << ":"
+		 << setw(2) << t.s;
+}
--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 int main(){
     struct Time t1,t2,t3;
-    cout<<"What time was it?";
+    cout<<"t1(h m s): ";
     getTime(t1);
-    cout<<"What time is it now?";
+    cout<<"t2(h m s): ";
     getTime(t2);
     t3=subtract(t2,t1); //t3=t2-t1
-    cout<<"Time diff is ";
+    cout<<"t3 ";
     display(t3);
 }
